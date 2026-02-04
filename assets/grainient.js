fb59832f6c96d7440feb0da82a82433cda57991d
sigenzo <109369295+sigenzo@users.noreply.github.com>
2026-02-04 16:00:04 +0000

    new file:   assets/grainient.js 	modified:   assets/index.js 	modified:   index.html

---
+++ assets/grainient.js
@@ -0,0 +1,11 @@
+const grainient = new Grainient({
+  element: "#grainient",
+  color1: "#FF9FFC",
+  color2: "#5227FF",
+  color3: "#B19EEF",
+  timeSpeed: 0.25,
+  warpStrength: 1,
+  grainAmount: 0.1,
+  contrast: 1.5,
+  zoom: 0.9,
+});
